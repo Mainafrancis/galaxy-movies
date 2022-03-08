3144924280ad5dd3779a710d4eaa6c3609bae95b
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-08 14:48:31 +0300

    play button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 5000,
       disableOnInteraction: false,
     },
     pagination: {
